e4b2dd07b11c9859409cce33104ef04511007286
AddagiriSnehaRoyal <sneharoyaladdagiri@gmail.com>
2026-08-07 18:13:01 +0530

    Create frontend foundation

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,21 @@
+import "./Home.css";
+
+function Home() {
+  return (
+    <main className="home">
+      <div className="home-content">
+        <h1>A little box of Love</h1>
+
+        <p>
+          Something tiny, made with you in mind.
+        </p>
+
+        <button className="create-button">
+          ✨ Create My Box
+        </button>
+      </div>
+    </main>
+  );
+}
+
+export default Home;
